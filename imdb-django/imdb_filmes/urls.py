a6953d2000f3e4edec0e41590516031fa4fce9f2
Augusto Grabe <67308025+augustograbe@users.noreply.github.com>
2022-07-23 19:25:33 -0300

    Add files via upload

--- imdb-django/imdb_filmes/urls.py
+++ imdb-django/imdb_filmes/urls.py
@@ -5,8 +5,12 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("rank_diretor", views.rank_diretor, name="rank_diretor"),
+    path("rank_ator", views.rank_ator, name="rank_ator"),
+    path("rank_roterista", views.rank_roterista, name="rank_roterista"),
     path("filme/<str:entry>", views.filme, name="filme"),
-    path("search/", views.search, name="search"),
+    path("pessoa/<str:entry>", views.pessoa, name="pessoa"),
+    path("pesquisa/", views.pesquisa, name="pesquisa"),
 ]
 
 handler404 = ""
--- imdb-django/imdb_filmes/views.py
+++ imdb-django/imdb_filmes/views.py
@@ -1,3 +1,30 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, "imdb_filmes/index.html")
+
+def rank_diretor(request):
+    return render(request, "imdb_filmes/rank_diretor.html")
+
+def rank_ator(request):
+    return render(request, "imdb_filmes/rank_ator.html")
+
+def rank_roterista(request):
+    return render(request, "imdb_filmes/rank_roterista.html")
+
+def filme(request, name):
+    return render(request, "imdb_filmes/filme.html", {
+
+    } )
+
+def pessoa(request, name):
+    return render(request, "imdb_filmes/pessoa.html", {
+        
+    } )
+
+def pesquisa(request):
+        return render(request, "imdb_filmes/pesquisa.html", {
+        
+    } )
